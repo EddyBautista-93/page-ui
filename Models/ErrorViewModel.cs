b88dbb4863c4e03be6560c100a0b4d445fa4fbf6
EddyBautista <bautista.j.eddy@gmail.com>
2021-05-27 18:07:54 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace page_ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
